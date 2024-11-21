d300da709db30f89796df7319106d524989edde2
Davidedino002 <dgreco@studentiapuliadigitalmaker.it>
2024-11-21 18:18:51 +0100

    Esercizio1 Javascript

---
+++ esercizio1.js
@@ -0,0 +1,9 @@
+// Definisci un valore predefinito per il numero di volte
+const numeroDiVolte = 10;
+
+// Ciclo per stampare il messaggio il numero specificato di volte
+for (let i = 1; i <= numeroDiVolte; i++) {
+    console.log(`Messaggio ${i}`);
+}
+
+    
